539483b8412fae14ced017d35948aa19065e4aed
Fernando Paolieri <cronuxs@gmail.com>
2026-06-16 19:53:12 +0100

    direnv support

---
+++ nix-config/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Benoit Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            # Go toolchain (matches the lovable monorepo: pkgs.go_1_26)
+            pkgs.go_1_26
+          ];
+        };
+      }
+    );
+}
